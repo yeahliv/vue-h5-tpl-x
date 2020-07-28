a7a15beb8c836cf44502027cbffc4b20e276a464
LivYe <951946936@qq.com>
2020-07-28 18:05:36 +0800

    fix: scss && vw

--- src/view/HelloWorld.vue
+++ src/view/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{msg}} {{mk}}</h1>
+    <img src="../assets/logo.png">
+    <h1>{{msg}} <span>{{mk}}</span></h1>
     <h2>This is a vue h5 project template</h2>
   </div>
 </template>
@@ -16,8 +17,13 @@ export default {
   },
   methods: {
     async get1() {
-      let { data: res } = await this.$get('http://rap2.taobao.org:38080/app/mock/262186//hello-world',{ tag: true })
-      let {code, data, success, msg} = res
+      let {
+        data: res
+      } = await this.$get(
+        'http://rap2.taobao.org:38080/app/mock/262186//hello-world',
+        { tag: true }
+      )
+      let { code, data, success, msg } = res
 
       if (success) {
         // success
@@ -27,13 +33,12 @@ export default {
       }
     },
 
-
     // When I need to make an HTTP request for data, I typically use the following approach
     // Copy the code snippet directly and modify the part east
     // 当我需要获取数据时，我通常都是封装好再调用，以下面这个代码块直接复制过去会写得更快
     async get2() {
       let { data: res } = await this.$api.hl.getHelloWorld({ tag: true })
-      let {code, data, success, msg} = res
+      let { code, data, success, msg } = res
 
       if (success) {
         // success
@@ -45,7 +50,7 @@ export default {
 
     async mk4396() {
       let { data: res } = await this.$api.hl.mk4396({ tag: true })
-      let {code, data, success, msg} = res
+      let { code, data, success, msg } = res
 
       if (success) {
         // success
@@ -66,10 +71,17 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1,
-h2 {
+<style lang="scss" scoped>
+h1 {
+  font-size: 36px;
   font-weight: normal;
+  padding-bottom: 20px;
+  span {
+    color: #42b983;
+  }
+}
+h2 {
+  font-size: 30px;
 }
 ul {
   list-style-type: none;
